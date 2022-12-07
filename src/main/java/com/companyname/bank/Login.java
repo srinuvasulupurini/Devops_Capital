4a2d3b1649b1f4df8c466d499c48c32a054a9715
srinuvasulupurini <srinuvasulu.purini@gmail.com>
2022-12-08 04:34:08 +0530

    web hook chnages
    Signed-off-by: srinuvasulupurini <srinuvasulu.purini@gmail.com>

--- src/main/java/com/companyname/bank/Login.java
+++ src/main/java/com/companyname/bank/Login.java
@@ -8,6 +8,6 @@ public class Login
 {
     public static void main( String[] args )
     {
-        System.out.println( "New changes in the Program" );
+        System.out.println( "New changes in the Program for github web hook" );
     }
 }
