489538525bc10c9e8f2215f60d763e36fe3bc71e
srinuvasulupurini <srinuvasulu.purini@gmail.com>
2022-12-08 04:12:57 +0530

    new changes
    Signed-off-by: srinuvasulupurini <srinuvasulu.purini@gmail.com>

--- src/main/java/com/companyname/bank/Login.java
+++ src/main/java/com/companyname/bank/Login.java
@@ -8,6 +8,6 @@ public class Login
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Java Program!" );
+        System.out.println( "New changes in the Program" );
     }
 }
